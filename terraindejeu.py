dba3ec776074ab2eaaa6aa3c80f47b6824faf749
uvsq22000514 <72227616+uvsq22000514@users.noreply.github.com>
2021-04-30 14:42:19 +0200

    Update terraindejeu.py

--- terraindejeu.py
+++ terraindejeu.py
@@ -8,7 +8,7 @@
 
 import tkinter as tk
 from random import sample
-Colors=["green2 ", "cyan"]
+import random as rd
 def generateur_case(p, n):
 	units=[(line, col) for col in range(n) for line in range(n)]
 	ntrees=int(n**2*p)
@@ -17,17 +17,15 @@ def generateur_case(p, n):
 	for (i, j) in trees:
 	    states[i][j]=1
 	return states
-def fill(states): 
-    n=len(states)
-    for line in range(n):
-        for col in range(n):
-            fill_cell(states, line, col)
-def fill_cell(states, line, col):
-        A=(unit*col, unit*line)
-        B=(unit*(col+1), unit*(line+1))
-        state=states[line][col]
-        color=Colors[state]
-        canvas.create_rectangle(A, B, fill=color, outline='black')
+
+def coul_quad():
+	for i in range(50):
+		for j in range(50):
+			r = rd.choice([0, p])
+			if r == p:
+				case=tk.Canvas(root, height = HEIGHT, width = WIDTH, bg="blue").grid(row = i, column = j)
+			else :
+				case=tk.Canvas(root, height = HEIGHT, width = WIDTH, bg="brown").grid(row = i, column = j)
 
 HEIGHT=10
 WIDTH=10
@@ -38,6 +36,7 @@ unit=10
 root= tk.Tk()
 root.title('Terrain de jeu')
 canvas= tk.Canvas(root, height=HEIGHT, width=WIDTH, bg='white')
+canvas= tk.Canvas(root, height=HEIGHT, width=WIDTH, bg='blue')
 canvas.grid(row = 0, column = 0, columnspan=2, padx=3, pady=3)
 for i in range(50):
             for j in range(50):
@@ -46,5 +45,5 @@ states = generateur_case(p, n)
 i=n//2
 j=0
 states[i][j]=2
-fill(states)
+coul_quad()
 root.mainloop()
