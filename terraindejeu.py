6825bc0a3721f21ca79c92b69258481cf2758d31
uvsq22000514 <72227616+uvsq22000514@users.noreply.github.com>
2021-05-23 00:56:25 +0200

    Update terraindejeu.py

--- terraindejeu.py
+++ terraindejeu.py
@@ -11,17 +11,17 @@ import tkinter as tk
 import random 
 
 ###Constantes###
-HEIGHT=50
-WIDTH=50
-
-cote= 10
+HEIGHT=int(input("Entrer un nombre"))
+WIDTH=int(input("Entrer un nombre"))
+T= int(input("Entrer un nombre"))
 p= 0.5
-T= 5
+cote= 10
 couleur=["green","blue"]
 eau= 1
 terre= 0
-nb_col= WIDTH// cote
-nb_lig= HEIGHT// cote
+nb_terre= HEIGHT*WIDTH
+nb_eau= 0
+
 # Matrices #
 #Mémorise les cellules#
 cell = [[0 for row in range(HEIGHT)] for col in range(WIDTH)] 
@@ -31,6 +31,16 @@ etat = [[terre for row in range(HEIGHT)] for col in range(WIDTH)]
 temp = [[terre for row in range(HEIGHT)] for col in range(WIDTH)]
 
 # Fonctions du programme #
+
+def dessiner():
+    """ Dessine les cellules """
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            if etat[x][y]==1:
+                coul = "blue"
+            else:
+                coul = "green"
+            canvas.itemconfig(cell[x][y], fill=coul)
 
 def quad():
     """ Quadrille le terrain et place des cellules de terre en fonction de p"""
@@ -47,85 +57,75 @@ def quad():
             temp[x][y] = etat[x][y]
             cell[x][y] = canvas.create_rectangle((x*cote, y*cote, 
                         (x+1)*cote, (y+1)*cote), outline="gray", fill=couleur[etat[x][y]])
-
-
-
-def dessiner():
-    """ Dessine les cellules """
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            if etat[x][y]==1:
-                coul = "blue"
-            else:
-                coul = "green"
-            canvas.itemconfig(cell[x][y], fill=coul)      
-
-def case_eau(a,b):
-    """ Compte les cases d'eau """
-    cpt= 0
-    if etat[(a-1)%WIDTH][(b+1)%HEIGHT] == 1:
-       cpt += 1
-    if etat[a][(b+1)%HEIGHT] == 1:
-        cpt += 1
-    if etat[(a+1)%WIDTH][(b+1)%HEIGHT] == 1:
-        cpt += 1
-    if etat[(a-1)%WIDTH][b] == 1:
-        cpt += 1
-    if etat[(a+1)%WIDTH][b] == 1:
-        cpt += 1
-    if etat[(a-1)%WIDTH][(b-1)%HEIGHT] == 1:
-        cpt += 1
-    if etat[a][(b-1)%HEIGHT] == 1:
-        cpt += 1
-    if etat[(a+1)%WIDTH][(b-1)%HEIGHT] == 1:
-        cpt += 1
-    return cpt
-    
-
-
-def calculer():
-    """ Création des cases d'eau et de terre en fonction du voisinage"""
-    a = random.randint(0, HEIGHT)
-    b = random.randint(0, WIDTH)
-    nb_voisins = case_eau(a, b)
-    n= 0
-    while n != 4:
-        for y in range(HEIGHT):
-            for x in range(WIDTH):
-
-# Naissance d'une case de terre #
-                if etat[x][y] == eau and nb_voisins < T :
-                    temp[x][y] = terre
-
-                elif etat[x][y] == terre and nb_voisins < T :
-                    temp[x][y] = terre
-    
-# Naissance d'une case d'eau #
-                if etat[x][y] == terre and T <= nb_voisins :
-                    temp[x][y] = eau
-
-                if etat[x][y] == eau and T <= nb_voisins :
-                    temp[x][y] = eau
-    
-                for y in range(HEIGHT):
-                    for x in range(WIDTH):
-                        etat[x][y] = temp[x][y]
-        n = n+1
-
-
-def perso():
-    x= random.randint(0, WIDTH)
-    y= random.randint(0, HEIGHT)
-    cell[x][y] = canvas.create_rectangle((x*cote, y*cote, 
-                        (x+1)*cote, (y+1)*cote), outline="gray", fill="black")
-
-
-
-def tableau():
-    """ Calcul et dessin du prochain tableau """
-    calculer()
     dessiner()
-    perso()
+
+
+def nb_voisin(a,b):
+    """Compte les cases voisisnes"""
+    voisin = random.randrange(8)
+    if voisin==1:
+        couleur = etat[(a-1)%WIDTH][(b+1)%HEIGHT]
+    elif voisin==2:
+        couleur = etat[a][(b+1)%HEIGHT]
+    elif voisin==3:
+        couleur = etat[(a+1)%WIDTH][(b+1)%HEIGHT]
+    elif voisin==4:
+        couleur = etat[(a-1)%WIDTH][b]
+    elif voisin==5:
+        couleur = etat[(a+1)%WIDTH][b]
+    elif voisin==6:
+        couleur = etat[(a-1)%WIDTH][(b-1)%HEIGHT]
+    elif voisin==7:
+        couleur = etat[a][(b-1)%HEIGHT]
+    else:
+        couleur = etat[(a+1)%WIDTH][(b-1)%HEIGHT]
+    return couleur
+    
+
+def nouvel_etat():
+    """Dessine le nouvel etat des cellules en fonction des voisins"""
+    global nb_terre, nb_eau
+    x = random.randrange(HEIGHT)
+    y = random.randrange(WIDTH)
+    nouvelle_cell = nb_voisin(x,y)
+    if etat[x][y] != nouvelle_cell:
+        if nouvelle_cell == terre:
+            nb_terre += 1
+            nb_eau -= 1
+        else :
+            nb_terre -= 1
+            nb_eau += 1
+            etat[x][y] = nouvelle_cell
+        if etat[x][y] == terre:
+            coul = "green"
+        else:
+            coul = "blue"
+        canvas.itemconfig(cell[x][y], fill=coul)
+
+
+def coord_to_lg(x, y):
+    """Fonction qui retourne la colonne et la ligne du quadrillage
+    à partir des coordonnées x et y"""
+    return x // cote, y // cote
+
+
+def perso(event):
+    """Place le personnage"""
+    i, j = coord_to_lg(event.x, event.y)
+    if etat[i][j] == -1:
+        x, y = i * cote, j * cote
+        carre = canvas.create_rectangle(x, y, x + cote,
+                                        y + cote, fill="black",
+                                        outline="gray")
+        etat[i][j] = carre
+    else:
+        canvas.delete(etat[i][j])
+        etat[i][j] = -1
+
+        
+def tableau():
+    """ Retourne du prochain tableau """
+    nouvel_etat()
     root.after(100, tableau)
       
                 
@@ -140,11 +140,12 @@ quad()
 tableau()
 
 
-# Liaison des evènenements #
-# canvas.bind("<keysym-80>", perso)
-# canvas.bind("<keysym-88>", perso)
-# canvas.bind("<keysym-83>", perso)
-#  canvas.bind("<keysym-85>", perso)
+ # Liaison des evènenements #
+canvas.bind("<Button-1>", perso)
+#canvas.bind("<Alt-Up>", perso)
+#canvas.bind("<Alt-Down>", perso)
+#canvas.bind("<Alt-Right>", perso)
+#canvas.bind("<Alt-Left>", perso)
 
 # Lancement de la boucle principal #
 root.mainloop() 
